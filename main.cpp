be93aede470c777612b10018db1081edd8eb9c0a
Arcana Delia Beatrice <deliabeatricearcana@yahoo.com>
2017-01-12 21:33:32 +0200

    Intrebare
    Am adaugat o functie care intreaba jucatorul in ce mod vrea sa joace:
    single player sau multiplayer

--- main.cpp
+++ main.cpp
@@ -38,3 +38,30 @@ void PrintBoard(char gameboard[3][3])
 	}
 	cout << endl << endl;
 }
+
+bool Question() {
+
+	while (1)
+	{ /**se repeta la infinit**/
+		while (1)
+            {
+			string choice;
+			int players = 0;
+			cout << "Would you like to play with 2 players or against a computer?" << endl;
+			cout << "Enter 1 for one player or 2 for two players: ";
+			cin >> choice;
+			cout << endl;
+			players = atoi(choice.c_str()); /**functia interpreteaza stringul ca fiind un numar intreg, care va fi returnat ca o valoare de tip int**/
+			if (players != 1 && players != 2)
+			{
+				cout << "Please enter either a 1 or a 2." << endl << endl;
+			}
+			else if (players == 1)
+				return false;
+			else if (players == 2)
+				return true;
+			break;
+		}
+	}
+}
+
